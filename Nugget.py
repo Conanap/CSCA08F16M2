46248b47b6a5adb6f11bdba1381d72d1669c84ec
Conanap <albionapc@gmail.com>
2016-11-11 17:26:12 -0500

    added __str__ method

--- Nugget.py
+++ Nugget.py
@@ -2,6 +2,9 @@ class Food:
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return 'no'
+
     def eat(self):
         print("You ran out of food!")
 
@@ -10,15 +13,20 @@ class Nugget(Food):
         Food.__init__(self, name)
         self.name += " nugget"
 
+    def __str__(self):
+        return 'oh bbg'
+
     def dip(self, sauce):
         print("mmmm " + sauce + " dip!")
 
 
 food = Food('chocolate')
 print(food.name)
+print(food)
 food.eat()
 
 chkn = Nugget('Chicken')
 print(chkn.name)
+print(chkn)
 chkn.dip('Plum')
 chkn.eat()
